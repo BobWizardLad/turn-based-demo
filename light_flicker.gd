7a56ab6d10409727cfd4610d36a2aaa3c97f6e0f
BobWizardLad <bobwizardlad@Gmail.com>
2025-02-22 12:38:07 -0600

    Lighting Flicker Finished

--- light_flicker.gd
+++ light_flicker.gd
@@ -4,9 +4,7 @@ var default_scale: float
 ## The last scale of this light; helps determine next scale
 var last_size: float
 ## Maximum amount of variance in the light flicker scale
-@export var light_range: float = 0.4
-
-signal flicker_finished
+@export var light_range: float
 
 func _ready():
 	default_scale = texture_scale
@@ -15,9 +13,9 @@ func _ready():
 
 func tween_light_flicker():
 	var tween = get_tree().create_tween()
+	tween.tween_callback(call_reflicker).set_delay(0.8)
 	var scale_target = randf_range(default_scale - light_range, default_scale + light_range)
-	print(scale_target)
-	tween.tween_property(self, "texture_scale", scale_target, 0.2)
+	tween.tween_property(self, "texture_scale", scale_target, 0.8)
 
-func _on_flicker_finished() -> void:
+func call_reflicker():
 	tween_light_flicker()
